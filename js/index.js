f58008974613b5f9d42ab93ff6c9846cc33eebd2
Alirezamxz <alirezamz.org@gmail.com>
2020-06-29 22:41:24 +0430

    js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-window.addEventListener('keydown', e => {
-    
-})
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; // if key is not set for audio stop function
+
+    audio.currentTime = 0; // play again from start
+
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
